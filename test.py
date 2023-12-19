be0bb12a2fc49e648a0247fb83f8f0f143532b42
Vanalio <tiziano.contorno@gmail.com>
2023-12-20 00:28:15 +0100

    test

--- test.py
+++ test.py
@@ -5,37 +5,20 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+from mpl_toolkits.mplot3d import Axes3D
 
-# Define transformations
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Lambda(lambda x: x.type(torch.double)),  # Cast to double
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalizing the images
-])
 
-# Check for CUDA and set device
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
 
-print(f"Using device: {device}")
-input("Press Enter to continue...")
-
-def clipped_relu(x):
-    return torch.clamp(x, min=0, max=1)
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # Initialize a placeholder for the first layer
         self.first_layer = None
-
-        # Create a sequence of fully connected layers
         self.layers = nn.ModuleList()
         layer_sizes = [3**i for i in range(net_magnitude, 0, -1)]
         for i in range(len(layer_sizes) - 1):
             layer = nn.Linear(in_features=layer_sizes[i], out_features=layer_sizes[i + 1])
-            nn.init.normal_(layer.weight, mean=0, std=1 / layer_sizes[i] ** 0.5)  # LeCun initialization
+            nn.init.normal_(layer.weight, mean=0, std=np.sqrt(0.1 / layer_sizes[i]))
             nn.init.constant_(layer.bias, 0.1)  # Initialize biases to 0.1
             self.layers.append(layer)
 
@@ -47,7 +30,7 @@ class Net(nn.Module):
         if self.first_layer is None:
             num_features = x.size(1)
             self.first_layer = nn.Linear(in_features=num_features, out_features=3**net_magnitude).to(device).double()
-            nn.init.normal_(self.first_layer.weight, mean=0, std=1 / num_features ** 0.5)  # LeCun initialization
+            nn.init.normal_(self.first_layer.weight, mean=0, std=np.sqrt(0.1 / num_features))
             nn.init.constant_(self.first_layer.bias, 0.1)  # Initialize biases to 0.1
 
         # Apply the first layer with clipped ReLU
@@ -55,19 +38,39 @@ class Net(nn.Module):
 
         # Iterate over all layers and apply them with clipped ReLU
         for layer in self.layers:
-            x = layer(x)
-            x = clipped_relu(x)
+            x = clipped_relu(layer(x))
 
         return x
 
-def loss(output):
-    ...
+
+def clipped_relu(x):
+    return torch.clamp(x, min=0, max=1)
+
+
+def repulsion_loss(outputs):
+    # Ensure outputs are in double precision
+    outputs = outputs.double() 
+
+    # Calculate pairwise distances (Euclidean)
+    pairwise_distances = torch.cdist(outputs, outputs, p=2)
+
+    # Increase penalty for small distances (especially for identical points)
+    # Example: using a smaller denominator for the exponential's exponent
+    repulsion = torch.exp(-pairwise_distances / 0.5)  # Adjust this value as needed
+
+    # Sum the penalties, avoiding self-comparison (diagonal elements)
+    mask = torch.eye(len(outputs), device=outputs.device).bool()
+    repulsion = repulsion.masked_fill(mask, 0)
+    
+    return repulsion.sum() / len(outputs)
+
 
 def print_parameter_summary(model):
     print("Parameter Summary:")
     for name, param in model.named_parameters():
         if param.requires_grad:
             print(f"{name} - Mean: {param.data.mean().item()}, Std: {param.data.std().item()}")
+
 
 def print_gradient_norms(model):
     print("Gradient Norms:")
@@ -80,114 +83,114 @@ def print_gradient_norms(model):
     total_norm = total_norm ** 0.5
     print(f"Total Norm: {total_norm}")
 
-def visualize_images(images):
-    # Number of images
-    num_images = len(images)
 
-    # Calculate the number of rows and columns to display the images
-    num_cols = int(np.sqrt(num_images))
-    num_rows = (num_images + num_cols - 1) // num_cols
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cpu")
+print(f"Using device: {device}")
+input("Press Enter to continue...")
 
-    # Set up the matplotlib figure and axes
-    fig, axs = plt.subplots(num_rows, num_cols, figsize=(num_cols * 2, num_rows * 2))
-    axs = axs.flatten()  # Flatten to 1D array for easy indexing
-
-    # Loop through each image
-    for i in range(num_images):
-        img = images[i].cpu().numpy()  # Convert to numpy array
-        img = np.transpose(img, (1, 2, 0))  # Rearrange dimensions from CHW to HWC
-        img = img * np.array((0.5, 0.5, 0.5)) + np.array((0.5, 0.5, 0.5))  # Unnormalize
-        img = img.clip(0, 1)  # Ensure the image is between 0 and 1
-
-        axs[i].imshow(img)
-        axs[i].axis('off')  # Turn off axis
-
-    # Hide any unused subplots
-    for i in range(num_images, len(axs)):
-        axs[i].axis('off')
-
-    plt.show()
-
-# Set the grid size
-batch_size = 64
+epochs = 50
+batch_size = 600
 print(f"Batch size: {batch_size}")
-
 net_magnitude = 9
 
-# Download and load the training data
-trainset = datasets.CIFAR10(root='./data', download=True, train=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=grid_size**3, shuffle=True)
+print_interval = 1
 
-net = Net().to(device).double()  # Cast to double
+
+#transform = transforms.Compose([
+#    transforms.ToTensor(),
+#    transforms.Lambda(lambda x: x.type(torch.double)),
+#    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+#])
+#trainset = datasets.CIFAR10(root='./data', download=True, train=True, transform=transform)
+#trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+# MNIST
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x.type(torch.double)),
+    transforms.Normalize((0.1307,), (0.3081,))  # Normalizing for MNIST
+])
+
+# Load the MNIST dataset
+trainset = datasets.MNIST(root='./data', download=True, train=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+
+net = Net().to(device).double()
 
 optimizer = optim.Adam(net.parameters())
 
-print_interval = 1  # How often to print the parameter summary
+
+plt.ion()  # Turn on interactive mode
+fig = plt.figure(figsize=(12, 6))
+
+# Subplot for 3D scatter plot
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Z')
+ax1.set_title('3D Points Visualization')
+ax1.view_init(elev=20, azim=20)
+ax1.invert_xaxis()
+
+
+# Subplot for loss plot
+ax2 = fig.add_subplot(122)
+ax2.set_xlabel('Batch')
+ax2.set_ylabel('Loss')
+ax2.set_title('Loss Over Batches')
+line, = ax2.plot([], [], 'r-', label='Repulsion Loss')
+ax2.legend()
+
+loss_values = []  # Initialize the list to store loss values
+
 
 # Training loop
-for epoch in range(2):
+for epoch in range(epochs):
     print(f"Starting epoch {epoch}:")
     for images, _ in trainloader:
-        images = images.to(device).double()  # Move data to GPU and cast to double
-
-        # Forward pass
-        #print("Forward pass")
+        images = images.to(device).double()
         output = net(images)
+
         print(f"Output size: {output.size()}")
-        print(f"Output first 3 items: {output[:3]}")
-        #print("Forwarded output")
-
-        # Calculate loss
+        print(f"Output first 10 items: {output[:10]}")
         print("Calculating loss")
-        loss = grid_based_loss(output, grid_size)
-        print(f"Loss: {loss.item()}")
+        loss_value = repulsion_loss(output)
+        print(f"Loss: {loss_value.item()}")
 
-        # Backward pass and optimization
-        #print("Backward pass")
-        #print("Zeroing gradients")
         optimizer.zero_grad()
-        #print("Zeroed gradients")
-        #print("Backwarding loss")
-        loss.backward()
-        #print("Backwarded loss")
-        #print("Stepping optimizer")
+        loss_value.backward()
         optimizer.step()
-        #print("Stepped optimizer")
 
-    # Print loss, parameter summary, and gradient norms at specified intervals
+        # Update loss plot
+        loss_value = repulsion_loss(output).item()  # Get loss as a Python float
+        loss_values.append(loss_value)
+        line.set_xdata(range(len(loss_values)))
+        line.set_ydata(loss_values)
+        ax2.relim()  # Recalculate limits
+        ax2.autoscale_view(True,True,True)
+
+        # Plotting the 3D points
+        ax1.clear()
+        output_np = output.cpu().detach().numpy()  # Convert output to numpy for plotting
+        ax1.scatter(output_np[:, 0], output_np[:, 1], output_np[:, 2])
+        ax1.set_xlabel('X')
+        ax1.set_ylabel('Y')
+        ax1.set_zlabel('Z')
+        ax1.set_title('3D Points Visualization')
+        ax1.view_init(elev=20, azim=20)
+        ax1.invert_xaxis()
+
+
+        plt.draw()
+        plt.pause(0.1)  # Pause to update the plot
+
     if epoch % print_interval == 0:
-        print(f"Epoch {epoch} - Loss: {loss.item()}")
         print_parameter_summary(net)
         print_gradient_norms(net)
+    
+    torch.save(net, 'model.pth')
 
-# Save the entire model
-torch.save(net, 'model.pth')
-
-'''
-# Load the entire model
-model = torch.load('model.pth')
-
-# Load a single batch of data
-images, _ = next(iter(trainloader))
-images = images.to(device).double()
-
-# Forward pass to get outputs
-with torch.no_grad():
-    output = model(images)
-
-# Determine subcube/subbox assignments
-subcube_assignments = determine_subcube_assignments(output, grid_size)
-
-# Convert subcube assignments to a format suitable for indexing
-subcube_assignments = subcube_assignments[:, 0] * (grid_size ** 2) + subcube_assignments[:, 1] * grid_size + subcube_assignments[:, 2]
-
-# Randomly select a subcube/subbox
-selected_subcube = random.choice(subcube_assignments.unique())
-
-# Get images corresponding to the selected subcube
-selected_images = images[subcube_assignments == selected_subcube]
-
-# Visualization code (using matplotlib or similar)
-# You need to implement visualize_images()
-visualize_images(selected_images.cpu())
-'''
+plt.ioff()  # Interactive mode off
+plt.show()  # Keep the plot open
